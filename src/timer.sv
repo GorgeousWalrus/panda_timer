355454d128e7ac040649e9078ff6afd2abe17a58
Luca Hanel <luca.hanel@hensoldt-cyber.com>
2020-08-27 17:36:12 +0200

    change to APB bus

--- src/timer.sv
+++ src/timer.sv
@@ -20,32 +20,34 @@
 // ------------------------------------------------------------
 
 `include "timer_incl.sv"
-`include "wb_intf.sv"
+`ifndef APB_BUS_SV
+`include "apb_intf.sv"
+`endif
 
 module timer(
-    input logic             clk,
-    input logic             rstn_i,
     output logic [1:0]      irq_o,
-    wb_bus_t.slave          wb_bus
+    apb_bus_t.slave         apb_bus
 );
 
 logic [31:0]    timer_regs_q[3];
 logic [31:0]    timer_regs_n[3];
 
 logic [31:0]    timer_incr;
-logic [31:0]    timer_do;
 
-assign wb_bus.wb_dat_sm = timer_do;
+logic [31:0]    PRDATA;
+logic           PREADY;
+
+assign apb_bus.PRDATA = PRDATA;
+assign apb_bus.PREADY = PREADY;
 
 always_comb
 begin
-    wb_bus.wb_ack = 1'b0;
-    wb_bus.wb_err = 1'b0;
+    PREADY = 1'b0;
+    PRDATA = 'b0;
+
     timer_regs_n = timer_regs_q;
-    // Timer
     timer_incr = 32'b0;
     irq_o = 'b0;
-    timer_do = 'b0;
 
     // only operate if enabled
     if(timer_regs_q[`CFG][`ENABLE_BIT]) begin
@@ -64,34 +66,20 @@ begin
             irq_o[1] = 1'b1;
     end
     
-    // WB slave
-    if(wb_bus.wb_cyc && wb_bus.wb_stb) begin
-        wb_bus.wb_ack = 1'b1;
-        if(wb_bus.wb_adr > 32'hc)
-            // If the address is out of bounds, return error
-            wb_bus.wb_err = 1'b1;
-        else begin
-            if(wb_bus.wb_we) begin
-                // Writing
-                if(wb_bus.wb_sel[0])
-                    timer_regs_n[wb_bus.wb_adr[3:2]][7:0] = wb_bus.wb_dat_ms[7:0];
-                if(wb_bus.wb_sel[1])
-                    timer_regs_n[wb_bus.wb_adr[3:2]][15:8] = wb_bus.wb_dat_ms[15:8];
-                if(wb_bus.wb_sel[2])
-                    timer_regs_n[wb_bus.wb_adr[3:2]][23:16] = wb_bus.wb_dat_ms[23:16];
-                if(wb_bus.wb_sel[3])
-                    timer_regs_n[wb_bus.wb_adr[3:2]][31:24] = wb_bus.wb_dat_ms[31:24];
-            end else begin
-                // Reading (only supports full 32 bit reading)
-                timer_do = timer_regs_q[wb_bus.wb_adr[3:2]];
-            end
-        end
+    // APB slave
+    if(apb_bus.PSEL && apb_bus.PENABLE) begin
+        PREADY = 1'b1;
+        if(apb_bus.PWRITE) // Write
+            timer_regs_n[apb_bus.PADDR[3:2]] = apb_bus.PWDATA;
+        else // Read
+            PRDATA = timer_regs_q[apb_bus.PADDR[3:2]];
     end
+
 end
 
-always_ff @(posedge clk, negedge rstn_i)
+always_ff @(posedge apb_bus.PCLK, negedge apb_bus.PRESETn)
 begin
-    if(!rstn_i) begin
+    if(!apb_bus.PRESETn) begin
         timer_regs_q[`TIMER] <= 'b0;
         timer_regs_q[`CFG] <= 'b0;
         timer_regs_q[`CMP] <= 'b0;
@@ -99,7 +87,6 @@ begin
         timer_regs_q[`TIMER] <= timer_regs_n[`TIMER];
         timer_regs_q[`CFG] <= timer_regs_n[`CFG];
         timer_regs_q[`CMP] <= timer_regs_n[`CMP];
-        // timer_regs_q[`TIMER] <= timer_regs_q[`TIMER] + timer_incr;
     end
 end
 
